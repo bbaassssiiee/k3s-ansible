e5f16e8216f743415e104a8b8abe5606e6d7fecb
Bas Meijer <bas.meijer@enexis.nl>
2025-04-18 10:58:00 +0200

    dependencies

--- k3s-wsl.sh
+++ k3s-wsl.sh
@@ -7,4 +7,7 @@ python3.12 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip wheel
 pip install -r requirements.txt
+ansible-galaxy collection install ansible.posix
+ansible-galaxy collection install community.general
 ./site.yml
+deactivate > /dev/null 2>&1
